46fb68a19af498dc053b318973d01a6c1c83bdab
Joby Pooppillikudiyil <61521479+devstackhub@users.noreply.github.com>
2022-07-18 16:35:13 +0200

    test

--- src/main/java/com/ibm/connector/controller/UserInfoController.java
+++ src/main/java/com/ibm/connector/controller/UserInfoController.java
@@ -2,6 +2,7 @@ package com.ibm.connector.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,10 @@ public class UserInfoController {
 	@PostMapping
 	public ResponseEntity<String> createLeadInSalesforce(@RequestBody UserInfo userInfo) {
 		return new ResponseEntity<String>("123456", HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "hellow world";
 	}
 }
